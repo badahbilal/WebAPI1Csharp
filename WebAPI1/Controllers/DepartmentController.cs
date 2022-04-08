f886b26afd90c3a9423e5162280123aac68dd325
BADAH Bilal <bilalbadah@gmail.com>
2022-04-08 18:09:49 +0000

    Add project files.

---
+++ WebAPI1/Controllers/DepartmentController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebAPI1.Models;
+using System.Data.Entity;
+
+namespace WebAPI1.Controllers
+{
+    public class DepartmentController : ApiController
+    {
+        EmployeeAPIEntities db = new EmployeeAPIEntities();
+
+        public HttpResponseMessage Get()
+        {
+            //var departments = db.Departments.Where(x => x.id == 5).FirstOrDefault();
+            var departments = db.Departments.ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, departments);
+        }
+
+
+        public string Post(Department department)
+        {
+
+           
+            try
+            {
+                db.Departments.Add(department);
+                db.SaveChanges();
+
+                return "Depratment added successfully";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+        public string Put(int id ,Department department)
+        {
+
+            
+
+            
+            try
+            {
+                Department dep = db.Departments.Find(id);
+                dep.name = department.name;
+                db.Entry(dep).State = EntityState.Modified;
+                db.SaveChanges();
+                return "Departement Updated successfully";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+        public string Delete(int id)
+        {
+
+            
+            try
+            {
+                Department deleDep = db.Departments.Find(id);
+                db.Departments.Remove(deleDep);
+                db.SaveChanges();
+
+                return "Departement Deleted successfully";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+    }
+}
---
+++ WebAPI1/Controllers/EmployeeController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using WebAPI1.Models;
+using System.Data;
+using System.Data.Entity;
+
+namespace WebAPI1.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        EmployeeAPIEntities db = new EmployeeAPIEntities();
+        public HttpResponseMessage Get()
+        {
+            var employee = db.Employees.ToList();
+
+            return Request.CreateResponse(HttpStatusCode.OK, employee);
+        }
+
+
+        public string Post(Employee employee)
+        {
+
+            try
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+
+                return "Employee added successfully";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+
+        public string Put(int id, Employee employee)
+        {
+
+            try
+            {
+                Employee updUmp = db.Employees.Find(id);
+                updUmp.Name = employee.Name;
+                updUmp.Department = employee.Department;
+                updUmp.MailID = employee.MailID;
+                updUmp.DOJ = employee.DOJ;
+
+                db.Entry(updUmp).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return "Departement Updated successfully";
+            }
+            catch(Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+
+        public string Delete(int id)
+        {
+
+            try
+            {
+                Employee deleUmp = db.Employees.Find(id);
+                db.Employees.Remove(deleUmp);
+                db.SaveChanges();
+
+                return "Departement Deleted successfully";
+            }
+            catch (Exception e)
+            {
+                return e.Message;
+            }
+
+        }
+    }
+}
